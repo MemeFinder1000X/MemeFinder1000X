ce6235c0d795c276809935e4848d70a2a0c3e1b6
MemeFinder1000X <buybrandings@gmail.com>
2026-09-08 12:30:18 +0100

    Restore security intelligence with on-chain authority module

--- security_intelligence.py
+++ security_intelligence.py
@@ -1,1 +1,4 @@
-test
+"""Compatibility entrypoint for MemeFinder security intelligence."""
+from security_intelligence_v2 import SecurityFinding, SecurityIntelligence
+
+__all__ = ["SecurityFinding", "SecurityIntelligence"]
